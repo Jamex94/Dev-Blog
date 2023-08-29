82b019e430c0d47feea3e02c106251de2f595cf3
James Trudeau <113483014+Jamex94@users.noreply.github.com>
2023-08-29 10:02:07 -0500

    page.tsx

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,155 +1,113 @@
-import React, { useState } from "react";
-import {
-  Header,
-  Main,
-  Section,
-  Text,
-  Image,
-  Badge,
-  Link,
-} from "@chakra-ui/react";
+export default withClient({
+  AboutMe: {
+    use client: true,
+  },
+});
 
-const AboutMe = () => {
-  const [isLoading, setIsLoading] = useState(true);
+export default function AboutMe() {
+  return (
+    <main className="max-w-5xl mx-auto p-4 sm:p-8 mb-16 grid gap-16">
+      <header className="flex flex-col items-center gap-2 py-8">
+        <h2> Hey there! I'm James.</h2>
+      </header>
+      <section>
+        <Text>
+          Exploring new technologies and developing software solutions.
+        </Text>
+      </section>
+      <section>
+        <Text>
+          Higher Technician in Multi-platform Applications Development.
+        </Text>
+      </section>
+      <section>
+        <Text>
+          Currently working as FrontEnd developer in [ArteRevelo](https://arterevelo.com).
+        </Text>
+      </section>
+      <section>
+        <Text>
+          Developing React apps and acquiring proficiency with TypeScript.
+        </Text>
+      </section>
+      <section>
+        <Text>
+          Pursuing 3D Modeling and Video editing as hobbies/side hustles.
+        </Text>
+      </section>
+      <section>
+        <ul>
+          <li>
+            <Badge icon="python">Python</Badge>
+          </li>
+          <li>
+            <Badge icon="java">Java</Badge>
+          </li>
+          <li>
+            <Badge icon="html5">HTML5</Badge>
+          </li>
+          <li>
+            <Badge icon="css3">CSS3</Badge>
+          </li>
+          <li>
+            <Badge icon="javascript">JavaScript</Badge>
+          </li>
+          <li>
+            <Badge icon="typescript">TypeScript</Badge>
+          </li>
+          <li>
+            <Badge icon="react">React</Badge>
+          </li>
+          <li>
+            <Badge icon="node.js">Node.js</Badge>
+          </li>
+          <li>
+            <Badge icon="mysql">MySQL</Badge>
+          </li>
+          <li>
+            <Badge icon="mongodb">MongoDB</Badge>
+          </li>
+          <li>
+            <Badge icon="git">Git</Badge>
+          </li>
+          <li>
+            <Badge icon="github">GitHub</Badge>
+          </li>
+          <li>
+            <Badge icon="visual-studio-code">Visual Studio Code</Badge>
+          </li>
+          <li>
+            <Badge icon="premiere">Adobe Premiere Pro</Badge>
+          </li>
+          <li>
+            <Badge icon="after-effects">Adobe After Effects</Badge>
+          </li>
+          <li>
+            <Badge icon="photoshop">Photoshop</Badge>
+          </li>
+          <li>
+            <Badge icon="blender">Blender</Badge>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <ul>
+          <li>
+            <Link href="https://www.linkedin.com/in/jamextrudeau/">
+              <Badge icon="linkedin">LinkedIn</Badge>
+            </Link>
+          </li>
+          <li>
+            <Link href="mailto:alex.trudeau@engineer.com">
+              <Badge icon="email">Email</Badge>
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
+}
 
-  const data = {
-    name: "James Trudeau",
-    about: "Exploring new technologies and developing software solutions.",
-    education: {
-      degree: "Higher Technician in Multi-platform Applications Development",
-    },
-    work: {
-      company: "ArteRevelo",
-      title: "FrontEnd developer",
-    },
-    skills: [
-      {
-        label: "Python",
-        icon: "python",
-      },
-      {
-        label: "Java",
-        icon: "java",
-      },
-      {
-        label: "HTML5",
-        icon: "html5",
-      },
-      {
-        label: "CSS",
-        icon: "css3",
-      },
-      {
-        label: "JavaScript",
-        icon: "javascript",
-      },
-      {
-        label: "TypeScript",
-        icon: "typescript",
-      },
-      {
-        label: "React",
-        icon: "react",
-      },
-      {
-        label: "Node.js",
-        icon: "node",
-      },
-      {
-        label: "MySQL",
-        icon: "mysql",
-      },
-      {
-        label: "MongoDB",
-        icon: "mongodb",
-      },
-      {
-        label: "Git",
-        icon: "git",
-      },
-      {
-        label: "GitHub",
-        icon: "github",
-      },
-      {
-        label: "Visual Studio Code",
-        icon: "visual-studio-code",
-      },
-      {
-        label: "Adobe Premiere Pro",
-        icon: "premiere",
-      },
-      {
-        label: "Adobe After Effects",
-        icon: "after-effects",
-      },
-      {
-        label: "Photoshop",
-        icon: "photoshop",
-      },
-      {
-        label: "Blender",
-        icon: "blender",
-      },
-    ],
-    social: {
-      linkedin: "https://www.linkedin.com/in/jamextrudeau/",
-      email: "alex.trudeau@engineer.com",
-    },
-  };
-
-  const renderData = () => {
-    if (isLoading) {
-      return <p>Loading...</p>;
-    } else {
-      return (
-        <Main>
-          <Header>
-            <h1>{data.name}</h1>
-          </Header>
-          <Section>
-            <Text>{data.about}</Text>
-          </Section>
-          <Section>
-            <Text>
-              <Badge icon={data.education.degree}>
-                {data.education.degree}
-              </Badge>
-            </Text>
-          </Section>
-          <Section>
-            <Text>
-              <Badge icon={data.work.company}>
-                {data.work.title}
-              </Badge>
-            </Text>
-          </Section>
-          <Section>
-            <ul>
-              {data.skills.map((skill) => (
-                <li key={skill.label}>
-                  <Badge icon={skill.icon}>{skill.label}</Badge>
-                </li>
-              ))}
-            </ul>
-          </Section>
-          <Section>
-            <ul>
-              {data.social.map((social) => (
-                <li key={social}>
-                  <Link href={social}>
-                    <Badge icon={social}>{social}</Badge>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Section>
-        </Main>
-      );
-    }
-  };
-
-  return renderData();
-};
-
-export default AboutMe;
+export default withClient({
+  AboutMe,
+});
